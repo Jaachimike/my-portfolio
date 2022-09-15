5c33fe689213fa381fe29236dceccd4dec4d91b2
somtee99 <okaforsomtoo007@gmail.com>
2022-09-15 17:08:06 +0100

    minor refactor

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -30,9 +30,9 @@ const NavBar = () => {
   ]
 
   return (
-    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
+    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black '>
       <div>
-        <h1 className='text-5xl font-signature ml-2'>Jaachimike</h1>
+        <h1 className='text-5xl font-signature ml-2'>Jaachimike Okafor</h1>
       </div>
 
       <ul className='hidden md:flex '>
