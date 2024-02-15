87823c918839643f6a8d8f93a5189487f987a7ba
Jaachi Okafor <okaforjaachi@gmail.com>
2024-02-15 22:37:26 +0100

    reduced name logo size on smaller screens

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black ">
       <div>
-        <h1 className="text-5xl font-signature font-bold ml-2">
+        <h1 className="text-2xl md:text-5xl font-signature font-bold ml-2">
           Jaachimike Okafor
         </h1>
       </div>
